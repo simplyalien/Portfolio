afa4c69642b1ed72496e7e64f27ad0c3524435d2
simplyalien <60414424+simplyalien@users.noreply.github.com>
2020-09-29 08:33:02 -0400

    alert function

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-$(".resume").on("click", function(){
-    alert("You have downloaded my resume!");
+$(document).ready(function() {
+    $(".resume").on("click", function(){
+        alert("You have downloaded my resume!");
+    });
+    $(".slide-selection").click(function() {
+        var linkhref = $(this).attr("href");
+        $("html, body").animate({
+            scrollTop: $(linkhref).offset().top
+        });
+    });
 });
